ec2deaa81f772abf687339db4a3387f61acc2a3b
Valerii Ivanov <valfrom@gmail.com>
2026-01-01 16:18:35 +0100

    Fix

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -9,21 +9,21 @@ var restore_player_position: bool = false
 
 # Adds 1 to score variable
 func add_score():
-        score += 1
+		score += 1
 
 # Loads next level
 func load_next_level(next_scene : PackedScene):
-        get_tree().change_scene_to_packed(next_scene)
+		get_tree().change_scene_to_packed(next_scene)
 
 func save_player_state(scene_path: String, player_position: Vector2):
-        saved_scene_path = scene_path
-        saved_player_position = player_position
-        restore_player_position = false
+		saved_scene_path = scene_path
+		saved_player_position = player_position
+		restore_player_position = false
 
 func mark_restore_player_position():
-        restore_player_position = true
+		restore_player_position = true
 
 func consume_restore_flag() -> bool:
-        var should_restore := restore_player_position
-        restore_player_position = false
-        return should_restore
+		var should_restore := restore_player_position
+		restore_player_position = false
+		return should_restore
