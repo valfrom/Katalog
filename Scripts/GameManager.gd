0297986f10ae7994a8e582b4f14ca60a7d04c164
Valerii Ivanov <valfrom@gmail.com>
2026-01-01 21:09:45 +0100

    Fix

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -30,126 +30,126 @@ var recently_unlocked_hero: String = ""
 var hero_unlock_return_scene: String = ""
 
 func _ready():
-    randomize()
-    _ensure_default_hero()
+	randomize()
+	_ensure_default_hero()
 
 # Adds 1 to score variable
 func add_score():
-    score += 1
+	score += 1
 
 # Loads next level
 func load_next_level(next_scene : PackedScene):
-    get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_packed(next_scene)
 
 func save_player_state(scene_path: String, player_position: Vector2):
-    saved_scene_path = scene_path
-    saved_player_position = player_position
-    restore_player_position = false
+	saved_scene_path = scene_path
+	saved_player_position = player_position
+	restore_player_position = false
 
 func mark_restore_player_position():
-    restore_player_position = true
+	restore_player_position = true
 
 func consume_restore_flag() -> bool:
-    var should_restore := restore_player_position
-    restore_player_position = false
-    return should_restore
+	var should_restore := restore_player_position
+	restore_player_position = false
+	return should_restore
 
 func _ensure_default_hero() -> void:
-    if unlocked_heroes.is_empty():
-        unlocked_heroes.append(HERO_ROSTER[0]["id"])
-        current_hero_id = HERO_ROSTER[0]["id"]
+	if unlocked_heroes.is_empty():
+		unlocked_heroes.append(HERO_ROSTER[0]["id"])
+		current_hero_id = HERO_ROSTER[0]["id"]
 
 func get_hero_definition(hero_id: String) -> Dictionary:
-    for hero in HERO_ROSTER:
-        if hero["id"] == hero_id:
-            return hero
-    return {}
+	for hero in HERO_ROSTER:
+		if hero["id"] == hero_id:
+			return hero
+	return {}
 
 func prepare_hero_run(selected_hero: String) -> void:
-    _ensure_default_hero()
-    if !unlocked_heroes.has(selected_hero):
-        selected_hero = unlocked_heroes[0]
+	_ensure_default_hero()
+	if !unlocked_heroes.has(selected_hero):
+		selected_hero = unlocked_heroes[0]
 
-    current_hero_queue = unlocked_heroes.duplicate()
-    current_hero_queue.erase(selected_hero)
-    current_hero_queue.push_front(selected_hero)
-    current_hero_id = selected_hero
-    emit_signal("hero_changed", current_hero_id)
+	current_hero_queue = unlocked_heroes.duplicate()
+	current_hero_queue.erase(selected_hero)
+	current_hero_queue.push_front(selected_hero)
+	current_hero_id = selected_hero
+	emit_signal("hero_changed", current_hero_id)
 
 func advance_to_next_hero() -> bool:
-    if current_hero_queue.size() <= 1:
-        current_hero_queue.clear()
-        current_hero_id = ""
-        return false
+	if current_hero_queue.size() <= 1:
+		current_hero_queue.clear()
+		current_hero_id = ""
+		return false
 
-    current_hero_queue.pop_front()
-    current_hero_id = current_hero_queue[0]
-    emit_signal("hero_changed", current_hero_id)
-    return true
+	current_hero_queue.pop_front()
+	current_hero_id = current_hero_queue[0]
+	emit_signal("hero_changed", current_hero_id)
+	return true
 
 func unlock_random_locked_hero() -> String:
-    var locked: Array[String] = []
-    for hero in HERO_ROSTER:
-        if !unlocked_heroes.has(hero["id"]):
-            locked.append(hero["id"])
+	var locked: Array[String] = []
+	for hero in HERO_ROSTER:
+		if !unlocked_heroes.has(hero["id"]):
+			locked.append(hero["id"])
 
-    if locked.is_empty():
-        return ""
+	if locked.is_empty():
+		return ""
 
-    var newly_unlocked := locked[randi() % locked.size()]
-    unlocked_heroes.append(newly_unlocked)
-    recently_unlocked_hero = newly_unlocked
-    return newly_unlocked
+	var newly_unlocked := locked[randi() % locked.size()]
+	unlocked_heroes.append(newly_unlocked)
+	recently_unlocked_hero = newly_unlocked
+	return newly_unlocked
 
 func prepare_hero_unlock(hero_id: String, return_scene: String) -> void:
-    recently_unlocked_hero = hero_id
-    hero_unlock_return_scene = return_scene
+	recently_unlocked_hero = hero_id
+	hero_unlock_return_scene = return_scene
 
 func consume_unlocked_hero() -> String:
-    var hero_id := recently_unlocked_hero
-    recently_unlocked_hero = ""
-    return hero_id
+	var hero_id := recently_unlocked_hero
+	recently_unlocked_hero = ""
+	return hero_id
 
 func consume_hero_unlock_return_scene() -> String:
-    var target_scene := hero_unlock_return_scene
-    hero_unlock_return_scene = ""
-    return target_scene
+	var target_scene := hero_unlock_return_scene
+	hero_unlock_return_scene = ""
+	return target_scene
 
 func register_level_player(player: Node) -> void:
-    _ensure_default_hero()
-    _active_level_player = player
-    if current_hero_queue.is_empty() and !unlocked_heroes.is_empty():
-        current_hero_queue = unlocked_heroes.duplicate()
-        current_hero_id = unlocked_heroes[0]
-    if !hero_changed.is_connected(_apply_current_hero):
-        hero_changed.connect(_apply_current_hero)
-    _apply_current_hero(current_hero_id)
+	_ensure_default_hero()
+	_active_level_player = player
+	if current_hero_queue.is_empty() and !unlocked_heroes.is_empty():
+		current_hero_queue = unlocked_heroes.duplicate()
+		current_hero_id = unlocked_heroes[0]
+	if !hero_changed.is_connected(_apply_current_hero):
+		hero_changed.connect(_apply_current_hero)
+	_apply_current_hero(current_hero_id)
 
 func _apply_current_hero(_hero_id: String) -> void:
-    if _active_level_player == null:
-        return
+	if _active_level_player == null:
+		return
 
-    var hero_id := _hero_id if _hero_id != "" else current_hero_id
-    var hero := get_hero_definition(hero_id)
-    if hero.is_empty():
-        return
+	var hero_id := _hero_id if _hero_id != "" else current_hero_id
+	var hero := get_hero_definition(hero_id)
+	if hero.is_empty():
+		return
 
-    if _active_level_player.has_method("apply_hero"):
-        _active_level_player.apply_hero(hero)
+	if _active_level_player.has_method("apply_hero"):
+		_active_level_player.apply_hero(hero)
 
 func handle_level_player_death(player: Node) -> void:
-    await player.death_tween()
+	await player.death_tween()
 
-    if advance_to_next_hero():
-        _apply_current_hero(current_hero_id)
-        await player.play_respawn()
-        return
+	if advance_to_next_hero():
+		_apply_current_hero(current_hero_id)
+		await player.play_respawn()
+		return
 
-    current_hero_queue.clear()
-    current_hero_id = ""
+	current_hero_queue.clear()
+	current_hero_id = ""
 
-    if saved_scene_path != "":
-        mark_restore_player_position()
-        SceneTransition.load_scene(saved_scene_path)
-    else:
-        await player.play_respawn()
+	if saved_scene_path != "":
+		mark_restore_player_position()
+		SceneTransition.load_scene(saved_scene_path)
+	else:
+		await player.play_respawn()
