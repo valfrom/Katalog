705801da8e4951c15fc6f404b57295c74e3db645
valfrom <valfrom@gmail.com>
2026-01-03 15:56:24 +0100

    Show hero portraits in HUD

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -3,8 +3,8 @@ extends Control
 @onready var score_texture = %Score/ScoreTexture
 @onready var score_label = %Score/ScoreLabel
 @onready var current_hero_label = %HeroHUD/CurrentHeroLabel
-@onready var available_heroes_label = %HeroHUD/AvailableHeroesLabel
 @onready var hero_lives_label = %HeroHUD/HeroLivesLabel
+@onready var available_heroes_icons = %HeroHUD/AvailableHeroesIcons
 
 func _ready():
     _update_hero_display()
@@ -20,18 +20,54 @@ func _update_hero_display() -> void:
         hero_name = hero_definition.get("name", GameManager.current_hero_id.capitalize())
     current_hero_label.text = "Hero: %s" % hero_name
 
-    var available_names: Array[String] = []
-    for hero_id in GameManager.unlocked_heroes:
-        var definition := GameManager.get_hero_definition(hero_id)
-        if definition.is_empty():
-            available_names.append(hero_id.capitalize())
-        else:
-            available_names.append(definition.get("name", hero_id.capitalize()))
-
-    if available_names.is_empty():
-        available_heroes_label.text = "Available: None"
-    else:
-        available_heroes_label.text = "Available: %s" % ", ".join(available_names)
+    _populate_available_hero_icons()
 
     var lives := GameManager.get_hero_lives(GameManager.current_hero_id)
     hero_lives_label.text = "Lives: %d" % lives
+
+func _populate_available_hero_icons() -> void:
+    for child in available_heroes_icons.get_children():
+        child.queue_free()
+
+    if GameManager.unlocked_heroes.is_empty():
+        var none_label := Label.new()
+        none_label.text = "None"
+        available_heroes_icons.add_child(none_label)
+        return
+
+    for hero_id in GameManager.unlocked_heroes:
+        var hero_definition := GameManager.get_hero_definition(hero_id)
+        var hero_name := hero_id.capitalize()
+        if not hero_definition.is_empty():
+            hero_name = hero_definition.get("name", hero_name)
+
+        var hero_portrait := _get_hero_portrait(hero_id, hero_definition)
+        var portrait_rect := TextureRect.new()
+        portrait_rect.custom_minimum_size = Vector2(32, 32)
+        portrait_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+        portrait_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+        portrait_rect.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+        portrait_rect.tooltip_text = hero_name
+
+        if hero_portrait != null:
+            portrait_rect.texture = hero_portrait
+        else:
+            portrait_rect.texture = score_texture.texture
+
+        available_heroes_icons.add_child(portrait_rect)
+
+func _get_hero_portrait(hero_id: String, hero_definition: Dictionary = {}) -> Texture2D:
+    if hero_definition.is_empty():
+        hero_definition = GameManager.get_hero_definition(hero_id)
+    if hero_definition.is_empty():
+        return null
+
+    var portrait_path: String = hero_definition.get("portrait_path", "")
+    if portrait_path == "":
+        return null
+
+    var portrait = load(portrait_path)
+    if portrait is Texture2D:
+        return portrait
+
+    return null
