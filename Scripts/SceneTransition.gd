8eede609d6fdf29e8a3dd6603963d58c2ef92fd8
valfrom <valfrom@gmail.com>
2026-01-03 18:10:30 +0100

    Reindent GDScript files (#25)

--- Scripts/SceneTransition.gd
+++ Scripts/SceneTransition.gd
@@ -10,19 +10,19 @@ enum state {FADE, SCALE}
 @export var transition_type : state
 
 func _ready():
-	dissolve_rect.hide() # Hide the dissolve rect
+    dissolve_rect.hide() # Hide the dissolve rect
 
 # You can call this funciton from any script by doing SceneTransition.load_scene(target_scene)
 func load_scene(target_scene: String):
-	match transition_type:
-		state.FADE:
-			transition_animation("fade", target_scene)
-		state.SCALE:
-			transition_animation("scale", target_scene)
+    match transition_type:
+        state.FADE:
+            transition_animation("fade", target_scene)
+        state.SCALE:
+            transition_animation("scale", target_scene)
 
 # This function handles the transition animation
 func transition_animation(animation_name: String, scene: String):
-	scene_transition_anim.play(animation_name)
-	await scene_transition_anim.animation_finished
-	get_tree().change_scene_to_file(scene)
-	scene_transition_anim.play_backwards(animation_name)
+    scene_transition_anim.play(animation_name)
+    await scene_transition_anim.animation_finished
+    get_tree().change_scene_to_file(scene)
+    scene_transition_anim.play_backwards(animation_name)
